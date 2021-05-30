1ed4dff06a0fd5357693b477708278dcbf0daca5
vbodige <vinay.bodige@interflora.co.uk>
2021-05-30 19:10:09 +0530

    updated the exe name

--- renderer/components/TreeComponent/TreeComponent.js
+++ renderer/components/TreeComponent/TreeComponent.js
@@ -4,67 +4,57 @@ import { Tree, Switch } from "antd";
 import { CarryOutOutlined, FormOutlined } from "@ant-design/icons";
 const treeData = [
   {
-    title: "parent 1",
+    title: "DemoProject",
     key: "0-0",
     icon: <CarryOutOutlined />,
     children: [
       {
-        title: "parent 1-0",
+        title: "Test Case1",
         key: "0-0-0",
         icon: <CarryOutOutlined />,
         children: [
           {
-            title: "leaf",
+            title: "Test suite 1",
             key: "0-0-0-0",
             icon: <CarryOutOutlined />,
           },
           {
-            title: (
-              <>
-                <div>multiple line title</div>
-                <div>multiple line title</div>
-              </>
-            ),
-            key: "0-0-0-1",
-            icon: <CarryOutOutlined />,
-          },
-          {
-            title: "leaf",
+            title: "Test suite 2",
             key: "0-0-0-2",
             icon: <CarryOutOutlined />,
           },
         ],
       },
       {
-        title: "parent 1-1",
+        title: "Test case 2",
         key: "0-0-1",
         icon: <CarryOutOutlined />,
         children: [
           {
-            title: "leaf",
+            title: "Test suite 2-1",
             key: "0-0-1-0",
             icon: <CarryOutOutlined />,
           },
         ],
       },
-      {
-        title: "parent 1-2",
-        key: "0-0-2",
-        icon: <CarryOutOutlined />,
-        children: [
-          {
-            title: "leaf",
-            key: "0-0-2-0",
-            icon: <CarryOutOutlined />,
-          },
-          {
-            title: "leaf",
-            key: "0-0-2-1",
-            icon: <CarryOutOutlined />,
-            switcherIcon: <FormOutlined />,
-          },
-        ],
-      },
+      // {
+      //   title: "parent 1-2",
+      //   key: "0-0-2",
+      //   icon: <CarryOutOutlined />,
+      //   children: [
+      //     {
+      //       title: "leaf",
+      //       key: "0-0-2-0",
+      //       icon: <CarryOutOutlined />,
+      //     },
+      //     {
+      //       title: "leaf",
+      //       key: "0-0-2-1",
+      //       icon: <CarryOutOutlined />,
+      //       switcherIcon: <FormOutlined />,
+      //     },
+      //   ],
+      // },
     ],
   },
   {
@@ -78,12 +68,12 @@ const treeData = [
         icon: <CarryOutOutlined />,
         children: [
           {
-            title: "leaf",
+            title: "Test suite 2-1",
             key: "0-1-0-0",
             icon: <CarryOutOutlined />,
           },
           {
-            title: "leaf",
+            title: "Test suite 2-2",
             key: "0-1-0-1",
             icon: <CarryOutOutlined />,
           },
